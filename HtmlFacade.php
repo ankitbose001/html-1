abb827cb52c74df2aeab1a1d91630c0fb67abb5f
Ankit Bose <49792804+ankitbose001@users.noreply.github.com>
2020-10-01 01:43:33 +0530

    Update HtmlFacade.php

--- HtmlFacade.php
+++ HtmlFacade.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @see \Illuminate\Html\HtmlBuilder
  */
-class HtmlFacade extends Facade {
+class HtmlFacade extends Facades {
 
 	/**
 	 * Get the registered name of the component.
 	 *
 	 * @return string
 	 */
-	protected static function getFacadeAccessor() { return 'html'; }
+	protected static function getFacadeAccessor() { return 'builder'; }
 
 }
